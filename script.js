5b1c6593237e310f07065c6e81f7d7027d2b7f60
dharshinipriya-k <priyadk1809@gmail.com>
2023-12-03 11:58:28 +0530

    script file updated

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 const pincode = document.getElementById("pin");
 const getDetails = document.getElementById("getDetails");
 
-getDetails.addEventListener("click", async () => {
-  //Using API to fetch details
-  const input = pincode.value;
-  const details = await fetch(`https://api.postalpincode.in/pincode/${input}`);
-  const data = await details.json();
+const promise1 = new Promise((resolve, reject) => {
+  resolve(success());
+  reject("Error fetching Data");
+})
+  .then(() => console.log())
+  .catch((error) => console.log(error));
 
-  displayData(data);
-});
+function success() {
+  getDetails.addEventListener("click", async () => {
+    //Using API to fetch details
+    const input = pincode.value;
+    const details = await fetch(
+      `https://api.postalpincode.in/pincode/${input}`
+    );
+    const data = await details.json();
 
-function displayData(data) {
-  const input = pincode.value;
-  console.log(data);
-  const displayDetails = document.createElement("div");
-  const contain = document.getElementById("contain");
-  contain.append(displayDetails);
-  displayDetails.setAttribute("id", "details");
-  for (var i = 0; i < data[0].PostOffice.length; i++) {
-    displayDetails.innerHTML = `<p><b>Pincode:</b> ${input} </p>
-                       <p><b>Name: </b> ${data[0].PostOffice[i].Name}</p>
-                       <p><b>Circle: </b> ${data[0].PostOffice[i].Circle}</p>
-                       <p><b>District: </b> ${data[0].PostOffice[i].District}</p>
-                       <p><b>Division: </b> ${data[0].PostOffice[i].Division}</p>
-                       <p><b>Block: </b> ${data[0].PostOffice[i].Block}</p>
-                       <p><b>State: </b> ${data[0].PostOffice[i].State}</p>
-                       <p><b>Type: </b> ${data[0].PostOffice[i].BranchType}</p>
-                       `;
+    displayData(data);
+  });
 
-    maindiv.append(displayDetails);
+  function displayData(data) {
+    const input = pincode.value;
+
+    const displayDetails = document.createElement("div");
+    const contain = document.getElementById("contain");
+    contain.append(displayDetails);
+    displayDetails.setAttribute("id", "details");
+    for (var i = 0; i < data[0].PostOffice.length; i++) {
+      displayDetails.innerHTML = `<p><b>Pincode:</b> ${input} </p>
+                         <p><b>Name: </b> ${data[0].PostOffice[i].Name}</p>
+                         <p><b>Circle: </b> ${data[0].PostOffice[i].Circle}</p>
+                         <p><b>District: </b> ${data[0].PostOffice[i].District}</p>
+                         <p><b>Division: </b> ${data[0].PostOffice[i].Division}</p>
+                         <p><b>Block: </b> ${data[0].PostOffice[i].Block}</p>
+                         <p><b>State: </b> ${data[0].PostOffice[i].State}</p>
+                         <p><b>Type: </b> ${data[0].PostOffice[i].BranchType}</p>
+                         `;
+
+      contain.append(displayDetails);
+    }
   }
 }
